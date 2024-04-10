b47326ba88eb13aa50f345333b9c76605d8f6711
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-04-10 19:18:14 +0530

    Update REPL to use evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"interpreter/lexer"
 	"interpreter/parser"
+    "interpreter/evaluator"
 	"io"
 )
 
@@ -30,8 +31,13 @@ func Start(input io.Reader, output io.Writer) {
             continue
         }
 
-        io.WriteString(output, program.String())
-        io.WriteString(output, "\n")
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(output, evaluated.Inspect())
+            io.WriteString(output, "\n")
+        }
+        // io.WriteString(output, program.String())
+        // io.WriteString(output, "\n")
         
         // for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
         //     fmt.Printf("%+v\n", tok)
