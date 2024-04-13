e3087966829f3bf757b5bc46e44687fe17f17faa
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-04-13 17:38:06 +0530

    add eval support for return statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,7 +14,7 @@ var (
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
     case *ast.Program:
-        return evalStatements(node.Statements)
+        return evalProgram(node.Statements)
     case *ast.ExpressionStatement:
         return Eval(node.Expression)
     case *ast.IntegerLiteral:
@@ -29,21 +29,41 @@ func Eval(node ast.Node) object.Object {
         right := Eval(node.Right)
         return evalInflixExpression(node.Operator, left, right)
     case *ast.BlockStatement:
-        return evalStatements(node.Statements)
+        return evalBlockStatement(node)
     case *ast.IfExpression:
         return evalIfExpression(node)
+    case *ast.ReturnStatement:
+        val := Eval(node.ReturnValue)
+        return &object.ReturnValue{Value: val}
     }
 
     return nil
 }
 
-func evalStatements(statements []ast.Statement) object.Object {
+func evalProgram(statements []ast.Statement) object.Object {
     var result object.Object
     
     for _, statement := range statements {
         result = Eval(statement)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
     }
 
+    return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+    var result object.Object
+
+    for _, statement := range block.Statements {
+        result = Eval(statement)
+
+        if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+            return result
+        }
+    }
     return result
 }
 
@@ -150,3 +170,4 @@ func isTruly(obj object.Object) bool {
         return true; 
     }
 }
+
